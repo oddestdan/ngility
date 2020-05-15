8803b1c5866e1403f8306920f4619fc093820c56
Denys Stetsenko2 <denys_stetsenko2@epam.com>
2020-05-15 14:46:32 +0300

    Update output path for Heroku server build dist

--- server.js
+++ server.js
@@ -14,17 +14,15 @@ const forceSSL = function() {
     next();
   }
 }
-// Instruct the app
-// to use the forceSSL
-// middleware
+// Instruct the app to use the forceSSL middleware
 app.use(forceSSL());
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/ngility'));
 
 // For all GET requests, send back index.html to use PathLocationStrategy
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ngility/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
